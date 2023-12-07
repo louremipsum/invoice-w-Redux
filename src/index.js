856d7b3f33c02c45f86a7088f34c451146243f85
louremipsum <myself.vinayak2@gmail.com>
2023-12-07 17:22:21 +0530

    setup: router + redux init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddInvoice from "./routes/AddInvoice";
+import ViewInvoice from "./routes/ViewInvoice";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ViewInvoice />,
+  },
+  {
+    path: "/create",
+    element: <AddInvoice />,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact page</div>,
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 reportWebVitals();
