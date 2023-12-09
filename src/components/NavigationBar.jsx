654f00fee417fb3b8767f91fc79c8bb74b3d1228
louremipsum <myself.vinayak2@gmail.com>
2023-12-10 00:07:46 +0530

    fix: replaced href to Link(router)

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -7,23 +7,28 @@ const NavigationBar = () => {
   return (
     <Navbar bg="dark" data-bs-theme="dark" sticky="top" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src="./favicon.ico"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Invoice Generator{" "}
-        </Navbar.Brand>
+        <Link to="/" className="navbar-brand">
+          <Navbar.Brand>
+            <img
+              alt=""
+              src="./favicon.ico"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Invoice Generator{" "}
+          </Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link href="#features" className="me-2">
+            <Link
+              to="/"
+              className="me-3 pt-1 text-body-secondary text-decoration-none"
+            >
               View Invoices
-            </Nav.Link>
+            </Link>
             <Link to="/create" type="button" className="btn btn-primary ">
               Create Invoice <IconPlus />
             </Link>
