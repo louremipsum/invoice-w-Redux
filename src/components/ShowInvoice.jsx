48f690506791f9d8f16033cfae6587f727d26522
louremipsum <myself.vinayak2@gmail.com>
2023-12-10 16:21:34 +0530

    refactor: moved GenerateInvoice and imported it fix: useNavigate to transfer state to /edit page for editing

--- src/components/ShowInvoice.jsx
+++ src/components/ShowInvoice.jsx
@@ -5,40 +5,23 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import Modal from "react-bootstrap/Modal";
-import { BiPaperPlane, BiCloudDownload } from "react-icons/bi";
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
-import { connect } from "react-redux";
-import { addItem } from "../features/invoices/invoiceSlice";
-import { v4 as uuidv4 } from "uuid";
+import GenerateInvoice from "../function/GenerateInvoice";
+import { BiCloudDownload, BiEdit } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
-function GenerateInvoice() {
-  html2canvas(document.querySelector("#invoiceCapture")).then((canvas) => {
-    const imgData = canvas.toDataURL("image/png", 1.0);
-    const pdf = new jsPDF({
-      orientation: "portrait",
-      unit: "pt",
-      format: [612, 792],
-    });
-    pdf.internal.scaleFactor = 1;
-    const imgProps = pdf.getImageProperties(imgData);
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-    pdf.save("invoice-001.pdf");
-  });
-}
 const ShowInvoice = (props) => {
-  const handleSaveInvoice = () => {
-    this.props.info.UID = uuidv4();
-    this.props.addItem(this.props.info); // Dispatch the addItem action with the current state as its payload
+  // Used useNavigate hook to navigate to edit page with props
+  const navigate = useNavigate();
+
+  const handleEditInvoice = () => {
+    navigate("/edit", { state: { info: props.info } });
   };
 
   return (
     <div>
       <Modal
-        show={this.props.showModal}
-        onHide={this.props.closeModal}
+        show={props.showModal}
+        onHide={props.closeModal}
         size="lg"
         centered
       >
@@ -46,17 +29,17 @@ const ShowInvoice = (props) => {
           <div className="d-flex flex-row justify-content-between align-items-start bg-light w-100 p-4">
             <div className="w-100">
               <h4 className="fw-bold my-2">
-                {this.props.info.billFrom || "John Uberbacher"}
+                {props.info.billFrom || "Louremipsum Inc."}
               </h4>
               <h6 className="fw-bold text-secondary mb-1">
-                Invoice #: {this.props.info.invoiceNumber || ""}
+                Invoice #: {props.info.invoiceNumber || ""}
               </h6>
             </div>
             <div className="text-end ms-4">
               <h6 className="fw-bold mt-1 mb-2">Amount&nbsp;Due:</h6>
               <h5 className="fw-bold text-secondary">
                 {" "}
-                {this.props.currency} {this.props.total}
+                {props.currency} {props.total}
               </h5>
             </div>
           </div>
@@ -64,19 +47,19 @@ const ShowInvoice = (props) => {
             <Row className="mb-4">
               <Col md={4}>
                 <div className="fw-bold">Billed to:</div>
-                <div>{this.props.info.billTo || ""}</div>
-                <div>{this.props.info.billToAddress || ""}</div>
-                <div>{this.props.info.billToEmail || ""}</div>
+                <div>{props.info.billTo || ""}</div>
+                <div>{props.info.billToAddress || ""}</div>
+                <div>{props.info.billToEmail || ""}</div>
               </Col>
               <Col md={4}>
                 <div className="fw-bold">Billed From:</div>
-                <div>{this.props.info.billFrom || ""}</div>
-                <div>{this.props.info.billFromAddress || ""}</div>
-                <div>{this.props.info.billFromEmail || ""}</div>
+                <div>{props.info.billFrom || ""}</div>
+                <div>{props.info.billFromAddress || ""}</div>
+                <div>{props.info.billFromEmail || ""}</div>
               </Col>
               <Col md={4}>
                 <div className="fw-bold mt-2">Date Of Issue:</div>
-                <div>{this.props.info.dateOfIssue || ""}</div>
+                <div>{props.info.dateOfIssue || ""}</div>
               </Col>
             </Row>
             <Table className="mb-0">
@@ -89,7 +72,7 @@ const ShowInvoice = (props) => {
                 </tr>
               </thead>
               <tbody>
-                {this.props.items.map((item, i) => {
+                {props.items?.map((item, i) => {
                   return (
                     <tr id={i} key={i}>
                       <td style={{ width: "70px" }}>{item.quantity}</td>
@@ -97,10 +80,10 @@ const ShowInvoice = (props) => {
                         {item.name} - {item.description}
                       </td>
                       <td className="text-end" style={{ width: "100px" }}>
-                        {this.props.currency} {item.price}
+                        {props.currency} {item.price}
                       </td>
                       <td className="text-end" style={{ width: "100px" }}>
-                        {this.props.currency} {item.price * item.quantity}
+                        {props.currency} {item.price * item.quantity}
                       </td>
                     </tr>
                   );
@@ -120,28 +103,28 @@ const ShowInvoice = (props) => {
                     SUBTOTAL
                   </td>
                   <td className="text-end" style={{ width: "100px" }}>
-                    {this.props.currency} {this.props.subTotal}
+                    {props.currency} {props.subTotal}
                   </td>
                 </tr>
-                {this.props.taxAmmount !== 0.0 && (
+                {props.taxAmmount !== 0.0 && (
                   <tr className="text-end">
                     <td></td>
                     <td className="fw-bold" style={{ width: "100px" }}>
                       TAX
                     </td>
                     <td className="text-end" style={{ width: "100px" }}>
-                      {this.props.currency} {this.props.taxAmmount}
+                      {props.currency} {props.taxAmmount}
                     </td>
                   </tr>
                 )}
-                {this.props.discountAmmount !== 0.0 && (
+                {props.discountAmmount !== 0.0 && (
                   <tr className="text-end">
                     <td></td>
                     <td className="fw-bold" style={{ width: "100px" }}>
                       DISCOUNT
                     </td>
                     <td className="text-end" style={{ width: "100px" }}>
-                      {this.props.currency} {this.props.discountAmmount}
+                      {props.currency} {props.discountAmmount}
                     </td>
                   </tr>
                 )}
@@ -151,14 +134,14 @@ const ShowInvoice = (props) => {
                     TOTAL
                   </td>
                   <td className="text-end" style={{ width: "100px" }}>
-                    {this.props.currency} {this.props.total}
+                    {props.currency} {props.total}
                   </td>
                 </tr>
               </tbody>
             </Table>
-            {this.props.info.notes && (
+            {props.info.notes && (
               <div className="bg-light py-3 px-4 rounded">
-                {this.props.info.notes}
+                {props.info.notes}
               </div>
             )}
           </div>
@@ -167,22 +150,22 @@ const ShowInvoice = (props) => {
           <Row>
             <Col md={6}>
               <Button
-                variant="primary"
-                className="d-block w-100"
-                onClick={this.handleSaveInvoice}
+                variant="outline-primary"
+                className="d-block w-100 mt-3 mt-md-0"
+                onClick={handleEditInvoice}
               >
-                <BiPaperPlane
-                  style={{ width: "15px", height: "15px", marginTop: "-3px" }}
+                <BiEdit
+                  style={{ width: "16px", height: "16px", marginTop: "-3px" }}
                   className="me-2"
                 />
-                Save Invoice
+                Edit Invoice
               </Button>
             </Col>
             <Col md={6}>
               <Button
                 variant="outline-primary"
                 className="d-block w-100 mt-3 mt-md-0"
-                onClick={GenerateInvoice}
+                onClick={() => GenerateInvoice(props.info.invoiceNumber)}
               >
                 <BiCloudDownload
                   style={{ width: "16px", height: "16px", marginTop: "-3px" }}
