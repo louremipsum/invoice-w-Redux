2da4f8884fb9d5365e18e167b33b28a76aec743f
louremipsum <myself.vinayak2@gmail.com>
2023-12-11 15:56:48 +0530

    add: Redux-persist to store the redux-store in localStorage. [why]: Rendering of errorElement, re-initialised the redux-store

--- src/index.js
+++ src/index.js
@@ -1,33 +1,40 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { store } from "./store";
-import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AddInvoice from "./routes/AddInvoice";
 import ViewInvoice from "./routes/ViewInvoice";
 import Edit from "./routes/Edit";
+import ErrorPage from "./routes/Error";
+import reportWebVitals from "./reportWebVitals";
+import { store, persistor } from "./store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <ViewInvoice />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/create",
     element: <AddInvoice />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/edit",
     element: <Edit />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/store.js
+++ src/store.js
@@ -1,8 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import invoiceReducer from "./features/invoices/invoiceSlice.js";
 
+/**
+ * Configuration object for persisting the Redux store.
+ * @type {Object}
+ * @property {string} key - The key under which the state will be stored.
+ * @property {Object} storage - The storage engine to be used for persisting the state. Here it is LocalStorage.
+ */
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+/**
+ * The persisted reducer for the invoice generator store.
+ */
+const persistedReducer = persistReducer(persistConfig, invoiceReducer);
+
+/**
+ * The store object for the invoice generator application.
+ */
 export const store = configureStore({
   reducer: {
-    invoices: invoiceReducer,
+    invoices: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
